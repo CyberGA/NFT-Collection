e0536d764ac6a5f50c7ae0b1c6a7dd1abad07fea
CyberGA <etomu.joshua@gmail.com>
2022-12-22 15:06:45 +0100

    contract deploy script

---
+++ CONTRACT/scripts/deploy.js
@@ -0,0 +1,27 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+const { ethers } = require("hardhat");
+require("dotenv").config({path: ".env"});
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("ls../constants");
+
+async function main() {
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  const metadataURL = METADATA_URL;
+
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs")
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(metadataURL, whitelistContract)
+
+  await deployedCryptoDevsContract.deployed()
+
+  console.log("Crypto Devs Contract Address: ", deployedCryptoDevsContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
