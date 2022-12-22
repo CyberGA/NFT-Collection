02cdcdade9e494d8a4226b4c95b7665b4c5287b9
CyberGA <etomu.joshua@gmail.com>
2022-12-22 15:07:48 +0100

    constants

---
+++ CONTRACT/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x91c56F23dB5fA9690D4913490E1D4D50599923F8";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
