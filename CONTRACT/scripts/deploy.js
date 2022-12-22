1d44fa6a663858fc456438952bf78dca4f058f11
CyberGA <etomu.joshua@gmail.com>
2022-12-23 00:51:20 +0100

    fix bug

--- CONTRACT/scripts/deploy.js
+++ CONTRACT/scripts/deploy.js
@@ -3,7 +3,7 @@
 //
 const { ethers } = require("hardhat");
 require("dotenv").config({path: ".env"});
-const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("ls../constants");
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
 async function main() {
   const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
